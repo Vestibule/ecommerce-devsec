4fd737832969c97a0f8ca953c70a5ac4791f7ea3
Corentin Prevost <corentinprevost@macbook-pro-de-nicolas-2.home>
2023-10-08 23:02:36 +0200

    wip

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/category');
 });
 
 Route::get('/category', function () {
